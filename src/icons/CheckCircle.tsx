6e2b547455863b810f185cb6045caf119a4d53a1
Wahid Rizka Fathurrohman <wahid.0310@students.amikom.ac.id>
2026-09-06 01:45:47 +0700

    fix(icons): align bell, dollar, information, check-circle with production
    Bell and Dollar now use the current production path geometry. Information and CheckCircle carry the circular data-glyph and evenodd clip-rule like production. Values taken from the live Geist SSR.

--- src/icons/CheckCircle.tsx
+++ src/icons/CheckCircle.tsx
@@ -9,10 +9,11 @@ export const CheckCircle = forwardRef<SVGSVGElement, IconProps>(
             height={size}
             width={size}
             data-slot="oxobz-icon"
+            data-glyph="circular"
             style={{ color: color === 'currentColor' ? 'currentColor' : `var(--ds-${color})`, ...style }}
             {...props}
         >
-            <path fill="currentColor" fillRule="evenodd" d="M14.5 8a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.47-1.47.53-.53L11 4.94l-.53.53L6.5 9.44l-.97-.97L5 7.94 3.94 9l.53.53 1.5 1.5c.3.3.77.3 1.06 0z"></path>
+            <path fill="currentColor" fillRule="evenodd" clipRule="evenodd" d="M14.5 8a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.47-1.47.53-.53L11 4.94l-.53.53L6.5 9.44l-.97-.97L5 7.94 3.94 9l.53.53 1.5 1.5c.3.3.77.3 1.06 0z"></path>
         </svg>
     ),
 );
--- src/icons/Information.tsx
+++ src/icons/Information.tsx
@@ -9,10 +9,11 @@ export const Information = forwardRef<SVGSVGElement, IconProps>(
             height={size}
             width={size}
             data-slot="oxobz-icon"
+            data-glyph="circular"
             style={{ color: color === 'currentColor' ? 'currentColor' : `var(--ds-${color})`, ...style }}
             {...props}
         >
-            <path fill="currentColor" fillRule="evenodd" d="M8 14.5a6.5 6.5 0 1 0 0-13 6.5 6.5 0 0 0 0 13M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16M6.25 7h1.5a1 1 0 0 1 1 1v4.25h-1.5V8.5h-1zM8 6a1 1 0 1 0 0-2 1 1 0 0 0 0 2"></path>
+            <path fill="currentColor" fillRule="evenodd" clipRule="evenodd" d="M8 14.5a6.5 6.5 0 1 0 0-13 6.5 6.5 0 0 0 0 13M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16M6.25 7h1.5a1 1 0 0 1 1 1v4.25h-1.5V8.5h-1zM8 6a1 1 0 1 0 0-2 1 1 0 0 0 0 2"></path>
         </svg>
     ),
 );
